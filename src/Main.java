4b7a715c61105ab6dd88296b7620716be7845c36
RandomBox <joaopedrogames2016@gmail.com>
2024-09-30 00:06:14 -0300

    New questions

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,13 @@ public class Main {
         System.out.println("UNIFAN Centro Universitário Alfredo Nasser");
         System.out.println("Aluno: João Pedro Gonçalves da Silva");
         System.out.println("Professor: Brenno Pimenta");
-        System.out.println("");
+        System.out.println(" ");
         System.out.println("Seja bem vindo ao Questionário de Jogos Clássicos! Você responderá 15 perguntas, cada uma " +
                 "delas tendo 5 alternativas com apenas uma sendo correta. Vamos começar, te desejo uma boa sorte!");
-        System.out.println("");
+        System.out.println(" ");
 
         Questao questao1 = new Questao();
-        questao1.pergunta = "Quem era o protagonista do primeiro jogo da franquia Street Fighter e em que ano ele foi " +
+        questao1.pergunta = "1) Quem era o protagonista do primeiro jogo da franquia Street Fighter e em que ano ele foi " +
                 "lançado?";
         questao1.opcaoA = "A) Protagonista Ken, ano 1987.";
         questao1.opcaoB = "B) Protagonista Ryu, ano 1989.";
@@ -20,7 +20,7 @@ public class Main {
         questao1.correta = "D";
 
         Questao questao2 = new Questao();
-        questao2.pergunta = "O famoso personagem Sonic tem seu visual baseado em um animal. Qual é esse animal?";
+        questao2.pergunta = "2) O famoso personagem Sonic tem seu visual baseado em um animal. Qual é esse animal?";
         questao2.opcaoA = "A) Gato.";
         questao2.opcaoB = "B) Porco-Espinho.";
         questao2.opcaoC = "C) Ouriço.";
@@ -29,7 +29,7 @@ public class Main {
         questao2.correta = "C";
 
         Questao questao3 = new Questao();
-        questao3.pergunta = "No primeiro jogo da franquia Mortal Kombat, havia uma luta secreta que exigia passos " +
+        questao3.pergunta = "3) No primeiro jogo da franquia Mortal Kombat, havia uma luta secreta que exigia passos " +
                 "específicos para ser acessada. Essa luta era contra qual personagem?";
         questao3.opcaoA = "A) Reptile.";
         questao3.opcaoB = "B) Shao Khan.";
@@ -39,7 +39,7 @@ public class Main {
         questao3.correta = "A";
 
         Questao questao4 = new Questao();
-        questao4.pergunta = "Qual é o nome do irmão do Mario que esteve presente desde Super Mario Bros?";
+        questao4.pergunta = "4) Qual é o nome do irmão do Mario que esteve presente desde Super Mario Bros?";
         questao4.opcaoA = "A) Bowser.";
         questao4.opcaoB = "B) Yoshi.";
         questao4.opcaoC = "C) Toad.";
@@ -48,13 +48,41 @@ public class Main {
         questao4.correta = "E";
 
         Questao questao5 = new Questao();
-        questao5.pergunta = "Qual era o nome do protagonista do primeiro Castlevania lançado em 1986?";
+        questao5.pergunta = "5) Qual era o nome do protagonista do primeiro Castlevania lançado em 1986?";
         questao5.opcaoA = "A) Simon.";
         questao5.opcaoB = "B) Trevor.";
         questao5.opcaoC = "C) Alucard.";
         questao5.opcaoD = "D) Hector.";
         questao5.opcaoE = "E) Isaac.";
         questao5.correta = "A";
+
+        Questao questao6 = new Questao();
+        questao6.pergunta = "6) O clássico jogo Doom de 1993 começa sua gameplay em uma base militar localizada em uma" +
+                " lua. Qual o nome dessa lua?";
+        questao6.opcaoA = "A) Deimos.";
+        questao6.opcaoB = "B) Phobos.";
+        questao6.opcaoC = "C) Triton.";
+        questao6.opcaoD = "D) Oberon.";
+        questao6.opcaoE = "E) Callisto.";
+        questao6.correta = "B";
+
+        Questao questao7 = new Questao();
+        questao7.pergunta = "7) Qual o nome do vilão principal de The Legend of Zelda de 1986?";
+        questao7.opcaoA = "A) Link.";
+        questao7.opcaoB = "B) Skull Kid.";
+        questao7.opcaoC = "C) Zant.";
+        questao7.opcaoD = "D) Ganon.";
+        questao7.opcaoE = "E) Veran.";
+        questao7.correta = "D";
+
+        Questao questao8 = new Questao();
+        questao8.pergunta = "8) Em qual jogo da franquia Mega Man ocorreu a primeira aparição do cão robótico Rush?";
+        questao8.opcaoA = "A) Mega Man 2.";
+        questao8.opcaoB = "B) Mega Man 7.";
+        questao8.opcaoC = "C) Mega Man 3.";
+        questao8.opcaoD = "D) Mega Man 5.";
+        questao8.opcaoE = "E) Mega Man 8.";
+        questao8.correta = "C";
 
         int acertos;
 
@@ -85,6 +113,20 @@ public class Main {
             acertos = acertos+1;
         }
 
+        questao6.escrevaQuestao();
+        if (questao6.isCorreta(questao6.leiaResposta())) {
+            acertos = acertos+1;
+        }
+
+        questao7.escrevaQuestao();
+        if (questao7.isCorreta(questao7.leiaResposta())) {
+            acertos = acertos+1;
+        }
+
+        questao8.escrevaQuestao();
+        if (questao8.isCorreta(questao8.leiaResposta())) {
+            acertos = acertos+1;
+        }
 
 
     }
